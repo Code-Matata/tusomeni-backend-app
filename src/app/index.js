1be6a3e8a8c4f831d415e021cfa1a7d33188602c
kibuikaCodes <kibuikasteve@gmail.com>
2023-03-12 15:53:09 +0300

    :sparkles: add chatGPT prompt route

--- src/app/index.js
+++ src/app/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(checkOrigins);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors({ origin: "*" }));
 
 // middleware for json 
 app.use(express.json());
